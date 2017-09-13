e921ba05c89de5415a384315fd977227e7fd2989
Forty7L <351264614@xiyanghui.com>
2017-09-13 16:15:57 +0800

    selenium

--- em.py
+++ em.py
@@ -14,18 +14,18 @@ def _format_addr(s):
     return formataddr((Header(name, 'utf-8').encode(), addr))
 
 def send(ImageBin):
-    # mine = MIMEBase('image','png',filename='verify.png')
-    # mine.add_header('Content-Disposition','attachment',filename = 'verify.png')
-    # mine.add_header('Content-ID','<0>')
-    # mine.add_header('X-Attachment-Id','0')
-    # mine.set_payload(ImageBin)
+    mine = MIMEBase('image','png',filename='verify.png')
+    mine.add_header('Content-Disposition','attachment',filename = 'verify.png')
+    mine.add_header('Content-ID','<0>')
+    mine.add_header('X-Attachment-Id','0')
+    mine.set_payload(ImageBin)
 
 
-    from_addr = '13288247797@163.com'
-    password = 'gwj123456'
+    from_addr = '351264614@xiyanghui.com'
+    password = 'zg8FaBvq4cH4fsCF'
     # to_addr = '351264614@qq.com'
     to_addr = '351264614@qq.com'
-    smtp_server = 'smtp.163.com'
+    smtp_server = 'smtp.exmail.qq.com'
 
     msg = MIMEMultipart()
     msg.attach(MIMEText('亲,请输入验证码', 'plain', 'utf-8'))
@@ -34,8 +34,12 @@ def send(ImageBin):
     msg['Subject'] = Header('来处理问题了啊', 'utf-8').encode()
     # encoders.encode_base64(mine)
     # msg.attach(mine)
-    server = smtplib.SMTP(smtp_server, 25)
+    # server = smtplib.SMTP(smtp_server, 465)
+    server = smtplib.SMTP_SSL(smtp_server, 465)
     server.set_debuglevel(1)
     server.login(from_addr, password)
     server.sendmail(from_addr, [to_addr], msg.as_string())
     server.quit()
+
+if __name__ == '__main__':
+    send(1)
