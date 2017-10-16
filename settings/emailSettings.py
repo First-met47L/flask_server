666552d6186143e9edcc177aa8a3de785bfe638c
Forty7L <351264614@xiyanghui.com>
2017-10-16 11:52:15 +0800

    update

--- settings/emailSettings.py
+++ settings/emailSettings.py
@@ -1,6 +1,6 @@
 #email settings
-email = '351264614@xiyanghui.com'
-password = 'zg8FaBvq4cH4fsCF'
-otherEmails = ['351264614@qq.com']
+email = 'yuchuan@xiyanghui.com'
+password = 'W4L5zvqjLce26zBv'
+otherEmails = ['rinky@xiyanghui.com']
 pop3_server = 'imap.exmail.qq.com'
 smtp_server = 'smtp.exmail.qq.com'
